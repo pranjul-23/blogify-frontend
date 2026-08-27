bf0e268ae3af298c05025bfdb5f9ba6888ed059d
pranjul-23 <pranjul231193@gmail.com>
2026-08-27 13:19:18 +0530

    secured cookies for production url

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -28,17 +28,9 @@ export async function POST(request) {
       message: data.message,
     });
 
-    // response.cookies.set("token", data.token, {
-    //   httpOnly: true,
-    //   secure: process.env.NODE_ENV === "production",
-    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    //   path: "/",
-    //   maxAge: 7 * 24 * 60 * 60, // seconds
-    // });
-
     response.cookies.set("token", data.token, {
       httpOnly: true,
-      secure: false,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       path: "/",
       maxAge: 7 * 24 * 60 * 60, // seconds
